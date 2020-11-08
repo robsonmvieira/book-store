a1609d854078fd4d0f053bb7db41d5fae37a4012
Robson Maia <robsonmvieira@gmail.com>
2020-11-08 16:11:36 -0300

    :sparkles: add statics books items shop

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  images = [944, 1011, 984, 900].map((n) => `https://picsum.photos/id/${n}/2000/500`);
+  // images = [1, 2, 3, 4]
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.images)
   }
 
 }
