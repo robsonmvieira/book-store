7128bdb7591cbab01d1d263cf08d48f6d74c33c8
Robson Maia <robsonmvieira@gmail.com>
2020-11-08 10:32:20 -0300

    :hammer: update core module

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    NgbModule,
+    CommonModule,
+    BrowserModule
+  ],
+  exports: [
+    NgModule,
+    BrowserModule,
   ]
 })
 export class CoreModule { }
