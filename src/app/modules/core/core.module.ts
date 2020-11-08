da18048bda031344a1308f23dbe6b9298da03e83
Robson Maia <robsonmvieira@gmail.com>
2020-11-08 16:10:13 -0300

    :sparkles: add new item to menu

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { PublicMenuComponent } from './components/public-menu/public-menu.component';
+import { RouterModule } from '@angular/router';
 
-
+// https://themeforest.net/item/bookoe-book-store-website-ui-design-psd-template/29272869
 @NgModule({
   declarations: [PublicMenuComponent],
   imports: [
-    NgbModule,
-    CommonModule,
     BrowserModule,
+    RouterModule,
+    NgbModule,
 
   ],
   exports: [
-    NgbModule,
-    CommonModule,
     BrowserModule,
+    NgbModule,
+    RouterModule,
 
     // components
     PublicMenuComponent,
