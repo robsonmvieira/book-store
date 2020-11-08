dd7bd3b136069b334d1bdac6b44058b2926aabb8
Robson Maia <robsonmvieira@gmail.com>
2020-11-08 16:09:40 -0300

    :bug: fix a imports errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
@@ -15,8 +17,9 @@ import { ContactComponent } from './pages/contact/contact.component';
     ContactComponent
   ],
   imports: [
+    CommonModule,
+    CoreModule,
     AppRoutingModule,
-    CoreModule
 
   ],
   providers: [],
