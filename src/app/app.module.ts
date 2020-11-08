8f1f71df3f41a26189be78da9437226216b166c8
Robson Maia <robsonmvieira@gmail.com>
2020-11-08 10:32:03 -0300

    :hammer: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './modules/core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
