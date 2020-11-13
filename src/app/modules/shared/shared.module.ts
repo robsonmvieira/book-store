7614c9949b49f172e4686dc7571dd368f45c5d48
Robson Maia <robsonmvieira@gmail.com>
2020-11-13 20:05:41 -0300

    :sparkles: add ngb bootstrap in shared module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule,
+  ],
+  exports: [
+    CommonModule,
+    NgbModule,
   ]
 })
 export class SharedModule { }
