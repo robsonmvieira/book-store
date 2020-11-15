5cbee131f74500d170ef3ef2aa7c59fdfdc572ae
Robson Maia <robsonmvieira@gmail.com>
2020-11-15 17:55:16 -0300

    :sparkles: add component service

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AuthorsComponent } from './pages/authors/authors.component';
 import { BooksComponent } from './pages/books/books.component';
+import ComponentService from '../shared/services/component.service';
 
 @NgModule({
   declarations: [HomeComponent, AuthorsComponent, BooksComponent],
   imports: [
     AdminRoutingModule,
     SharedModule
-  ]
+  ],
+  providers: [ComponentService]
 })
 export class AdminModule { }
