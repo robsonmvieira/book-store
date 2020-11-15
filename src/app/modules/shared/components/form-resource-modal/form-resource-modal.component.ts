78d3b988cda3e93261cb0ffb283147b5358eeeec
Robson Maia <robsonmvieira@gmail.com>
2020-11-15 17:59:53 -0300

    :sparkles: add method for modal component

--- src/app/modules/shared/components/form-resource-modal/form-resource-modal.component.ts
+++ src/app/modules/shared/components/form-resource-modal/form-resource-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form-resource-modal',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormResourceModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {
+  }
+
+  openModalFormResource(modal) {
+    this.modalService.open(modal, { scrollable: true });
   }
 
 }
