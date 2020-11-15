cbb10df2c932047bb361f339ea1c97535fafc79d
Robson Maia <robsonmvieira@gmail.com>
2020-11-15 17:58:44 -0300

    :sparkles: show modal if button new author clicked

--- src/app/modules/admin/pages/authors/authors.component.ts
+++ src/app/modules/admin/pages/authors/authors.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { FormResourceModalComponent } from './../../../shared/components/form-resource-modal/form-resource-modal.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import ComponentService from 'src/app/modules/shared/services/component.service';
+import { map, take, tap } from 'rxjs/operators'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-authors',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authors.component.scss']
 })
 export class AuthorsComponent implements OnInit {
+  subscription: Subscription
 
-  constructor() { }
+  @ViewChild('modalForm', { static: false }) modal: FormResourceModalComponent;
+  @ViewChild('authorForm', {static: false}) modalForm;
+  constructor(private router: Router, private componentService: ComponentService) { }
 
   ngOnInit(): void {
+
+    // this.modal.openModalFormResource()
+    this.subscription = this.componentService.newAuthorEvent
+    .pipe(
+      take(1)
+    )
+    .subscribe((ev: string) =>  {
+      if (ev.includes('new-author')) {
+        this.router.navigate(['/admin/autores']).then(() => {
+          this.modal.openModalFormResource(this.modalForm)
+        })
+      }
+    })
+
+  }
+  openModalFormResource(modal) {
+    this.modal.openModalFormResource(modal)
+  }
+
+  ngOnDestroy () {
+    this.subscription.unsubscribe()
   }
 
 }
