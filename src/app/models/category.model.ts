79b86065cf4862c7034b5d93163907e8c702dbd6
Robson Maia <robsonmvieira@gmail.com>
2020-11-19 19:37:14 -0300

    :sparkles: add method for convert json to Category

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -1,5 +1,10 @@
-import Base from './base.model';
+import { Base } from '../modules/shared/models/base.model';
 
-export default class Category extends Base {
+export default class Category extends Base  {
   name: string
+
+  static fromJson(json): Category {
+    const category = Object.assign(new Category(), json)
+    return category
+  }
 }
