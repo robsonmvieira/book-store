44d7321bd730e5efdce42feba3820b50f546e554
Robson Maia <robsonmvieira@gmail.com>
2020-11-19 17:15:03 -0300

    :memo: update base api url

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormResourceModalComponent } from './components/form-resource-modal/form-resource-modal.component';
-
+import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
@@ -10,13 +10,16 @@ import { FormResourceModalComponent } from './components/form-resource-modal/for
   imports: [
     CommonModule,
     NgbModule,
+    HttpClientModule,
   ],
   exports: [
     CommonModule,
     NgbModule,
+    HttpClientModule,
 
     // components
     FormResourceModalComponent,
-  ]
+  ],
+  providers: [HttpClientModule]
 })
 export class SharedModule { }
