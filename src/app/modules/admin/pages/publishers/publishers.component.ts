0233918e8628dc956788daa28dfc63be79b5830c
Robson Maia <robsonmvieira@gmail.com>
2020-12-03 20:13:28 -0300

    :sparkles: add method to open modal onClick

--- src/app/modules/admin/pages/publishers/publishers.component.ts
+++ src/app/modules/admin/pages/publishers/publishers.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
+import { PublisherFormComponent } from 'src/app/modules/publisher/form/publish-form.component';
 import { Publisher } from 'src/app/modules/publisher/publisher.model';
 import { PublisherService } from 'src/app/modules/publisher/publisher.service';
 
@@ -10,12 +12,18 @@ import { PublisherService } from 'src/app/modules/publisher/publisher.service';
 })
 export class PublishersComponent implements OnInit {
 
+  @ViewChild(PublisherFormComponent, {static: false}) modal: PublisherFormComponent
   publishers$: Observable<Publisher[]>
-  constructor(private publisherService: PublisherService) { }
+  constructor(private publisherService: PublisherService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
 
     this.publishers$ = this.publisherService.list()
+
+  }
+
+  openModal() {
+    this.modal.openFormModal()
 
   }
 
