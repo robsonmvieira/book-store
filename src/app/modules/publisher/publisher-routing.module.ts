5bd831520067f0c1e3a52195c7164144c20f1e2c
Robson Maia <robsonmvieira@gmail.com>
2020-12-06 09:25:24 -0300

    :sparkles: add new route in publisher module

--- src/app/modules/publisher/publisher-routing.module.ts
+++ src/app/modules/publisher/publisher-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PublisherDetalheComponent } from './pages/detalhe/publisher-detalhe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path :':id',
+    component: PublisherDetalheComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/publisher/publisher.module.ts
+++ src/app/modules/publisher/publisher.module.ts
@@ -3,14 +3,14 @@ import { SharedModule } from '../shared/shared.module';
 import { PublisherRoutingModule } from './publisher-routing.module';
 
 import { PublisherFormComponent } from './form/publish-form.component';
-
+import { PublisherDetalheComponent } from './pages/detalhe/publisher-detalhe.component';
 
 @NgModule({
-  declarations: [PublisherFormComponent],
+  declarations: [PublisherFormComponent, PublisherDetalheComponent],
   imports: [
     SharedModule,
     PublisherRoutingModule
   ],
-  exports: [PublisherFormComponent]
+  exports: [PublisherFormComponent, PublisherDetalheComponent]
 })
 export class PublisherModule { }
