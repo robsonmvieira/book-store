630055a1dcaf5ecb49ea75ed555e5edfcae4a9d4
Robson Maia <robsonmvieira@gmail.com>
2020-12-07 11:22:56 -0300

    :sparkles: became publish form dinamically

--- src/app/modules/publisher/form/publish-form.component.ts
+++ src/app/modules/publisher/form/publish-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Publisher } from '../publisher.model';
 
 @Component({
   selector: 'app-form-publisher',
@@ -8,15 +10,39 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class PublisherFormComponent implements OnInit {
 
+  publisher: Publisher
+  publisherForm: FormGroup
+  titleForm = ""
+  titleBtn = ""
   @ViewChild('longContent', {static: false}) modal;
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-
+    this.publisherFormBuilder()
   }
 
-  openFormModal () {
+
+
+  openFormModal (publisherEdit?: Publisher) {
+    if(publisherEdit) {
+      this.titleForm = "Editar Categoria"
+      this.titleBtn = "Atualizar"
+      this.publisherForm.patchValue(publisherEdit)
+    }
+    else {
+      this.titleForm = "Nova Categoria"
+      this.titleBtn = "Salvar"
+      this.publisherForm.reset()
+    }
     this.modalService.open(this.modal, {size: 'xl'})
+  }
+
+  private publisherFormBuilder() {
+    this.publisherForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      web_site: ['', [Validators.required]]
+    })
   }
 
 }
