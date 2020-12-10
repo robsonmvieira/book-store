4a583b6948809269eb6014496f6569bc6dbb391b
Robson Maia <robsonmvieira@gmail.com>
2020-12-10 16:42:54 -0300

    :art: add author properties

--- src/app/modules/author/author.model.ts
+++ src/app/modules/author/author.model.ts
@@ -1,6 +1,12 @@
+import { Book } from '../book/book.model';
 import { Base } from '../shared/models/base.model';
 
 export class Author extends Base {
+
+  name: string
+  email: string
+  books?: Book[]
+
 
   static fromJson(json): Author {
     const category = Object.assign(new Author(), json)
