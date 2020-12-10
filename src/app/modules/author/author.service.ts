5122900e86ab0266b779c9ce3eecb15f3826658f
Robson Maia <robsonmvieira@gmail.com>
2020-12-10 16:49:23 -0300

    :art: implemented inheritance on author service

--- src/app/modules/author/author.service.ts
+++ src/app/modules/author/author.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BaseResourceService } from '../shared/services/base-resource.service';
+import { Author } from './author.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthorService {
+export class AuthorService extends BaseResourceService<Author> {
 
-  constructor() { }
+  constructor( protected injector: Injector) {
+    super(`${environment.url}authors`, injector, Author.fromJson)
+   }
 }
