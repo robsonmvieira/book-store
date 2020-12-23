468cd9c48300efea9db7b393464bc5190d556072
Robson Maia <robsonmvieira@gmail.com>
2020-12-23 17:07:53 -0300

    :sparkles: add actions to author in state

--- src/app/state/authors/actions.ts
+++ src/app/state/authors/actions.ts
@@ -1,4 +1,17 @@
+import { Author } from "src/app/modules/author/author.model"
+import { EditAuthorDTO } from "./dtos"
+
 export class GetAllAuthors {
   static readonly type = '[Admin] Get all Authors'
   constructor() {}
+}
+
+export class CreateAuthor {
+  static readonly type = '[Admin] add new Author'
+  constructor(public payload: Author) {}
+}
+
+export class UpdateAuthor {
+  static readonly type = '[Admin] update Author'
+  constructor(public payload: EditAuthorDTO) {}
 }
