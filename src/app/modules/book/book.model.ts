71cb724c421a6552190991b7adeaac09af87ab42
Robson Maia <robsonmvieira@gmail.com>
2020-12-27 20:24:21 -0300

    :art: add property for book model

--- src/app/modules/book/book.model.ts
+++ src/app/modules/book/book.model.ts
@@ -1,6 +1,16 @@
+import { Author } from './../author/author.model';
 import { Base } from '../shared/models/base.model';
+import { Publisher } from '../publisher/publisher.model';
 
 export class Book extends Base {
+
+  title: string
+  authors: Author[]
+  publisher: Publisher
+  isbn10: string
+  isbn13: string
+  status: boolean
+  image?: string
 
   static fromJson(json): Book {
     const category = Object.assign(new Book(), json)
