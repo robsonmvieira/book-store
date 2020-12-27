b8894884f99f41c7181fd51e879d6e978674bda8
Robson Maia <robsonmvieira@gmail.com>
2020-12-26 23:58:51 -0300

    :art: apply inheritance from base services

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BaseResourceService } from '../shared/services/base-resource.service';
+import { Book } from './book.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BookService {
+export class BookService extends BaseResourceService<Book> {
 
-  constructor() { }
+  constructor(protected injector: Injector) {
+    super(`${environment.url}books`, injector, Book.fromJson);
+   }
 }
