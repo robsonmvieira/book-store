1f13dfaad30c87da48c3d97493789a6fbbaa4738
Robson Maia <robsonmvieira@gmail.com>
2021-02-01 23:54:15 -0300

    :sparkles: add book detail page

--- src/app/modules/book/book.module.ts
+++ src/app/modules/book/book.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
 import { FormComponent } from './form/form.component';
-
+import { BookDetailComponent } from './pages/book-detail/book-detail.component';
 
 @NgModule({
-  declarations: [FormComponent],
+  declarations: [FormComponent, BookDetailComponent],
   imports: [
     CommonModule,
     BookRoutingModule
