039f6f895da216c03dbe8ba79713a0beaed473a8
Robson Maia <robsonmvieira@gmail.com>
2021-02-12 10:57:43 -0300

    :sparkles: add createBook  in actionbook files

--- src/app/state/books/actions.ts
+++ src/app/state/books/actions.ts
@@ -1,5 +1,13 @@
+import { Book } from "src/app/modules/book/book.model"
+
 export class GetAllBooks {
   static readonly type = '[Admin] get all books'
 
   constructor() {}
+}
+
+export class CreateBook {
+  static readonly type = '[Admin] new book'
+
+  constructor(public payload: Book) {}
 }
